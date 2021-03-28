c87c1e1171642c7a6cef92a8bc59d90faaa3a4f1
Zohar Pfeffer <pfzohar@gmail.com>
2021-03-28 17:34:33 +0300

    Added router

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { join } from 'path'
 import cors from 'cors'
+import { router } from './router'
 
 export const app = express()
 app.use(cors())
 app.use(express.json())
-
 app.use(express.static(join(__dirname, 'public')))
+app.use(router)
