d71b670171a249adf08ca1cf715f82441a2a8fb6
Zohar Pfeffer <pfzohar@gmail.com>
2021-03-28 17:48:03 +0300

    Added angular app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import { join } from 'path'
+import path from 'path'
 import cors from 'cors'
 import { router } from './router'
 
 export const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../../public')))
 app.use(router)
--- server/src/router.ts
+++ server/src/router.ts
@@ -2,6 +2,6 @@ import { Router } from 'express'
 
 export const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello world')
+router.get('/ping', (req, res) => {
+  res.send('Pong')
 })
