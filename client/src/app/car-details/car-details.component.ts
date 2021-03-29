2c3c5f62f36a5bb569c42575eea364e4f2ef0e57
Zohar Pfeffer <pfzohar@gmail.com>
2021-03-29 21:45:13 +0300

    Moved card template into car-details component

--- client/src/app/car-details/car-details.component.ts
+++ client/src/app/car-details/car-details.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ICar } from '../car.entity';
 
 @Component({
+  selector: 'app-car-details',
   templateUrl: './car-details.component.html',
   styleUrls: ['./car-details.component.sass']
 })
 export class CarDetailsComponent implements OnInit {
-
+  @Input() car: ICar
   constructor() { }
 
   ngOnInit(): void {
