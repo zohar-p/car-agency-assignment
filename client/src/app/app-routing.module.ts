00a0af614da807954bd07fbf9204c0b035b2f262
Zohar Pfeffer <pfzohar@gmail.com>
2021-03-31 17:31:43 +0300

    component >> components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarDetailsComponent } from './pages/car-details/car-details.component';
-import { CarsListComponent } from './component/cars-list/cars-list.component';
+import { CarsListComponent } from './components/cars-list/cars-list.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarDetailsComponent } from './pages/car-details/car-details.component';
-import { CarItemComponent } from './component/car-item/car-item.component';
+import { CarItemComponent } from './components/car-item/car-item.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { CarsListComponent } from './component/cars-list/cars-list.component';
+import { CarsListComponent } from './components/cars-list/cars-list.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
