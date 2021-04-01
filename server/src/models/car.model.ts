28c40c6101a7d25f2e1d1be64303b7b00cb06f94
Zohar Pfeffer <pfzohar@gmail.com>
2021-04-02 00:05:37 +0300

    removed _id and __v from db response

--- server/src/models/car.model.ts
+++ server/src/models/car.model.ts
@@ -25,4 +25,12 @@ const carSchema = new Schema({
   }
 })
 
+carSchema.set('toJSON', {
+  transform: function (doc: any, ret: any) {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+  }
+}); 
+
 export const CarModel = model('Car', carSchema)
