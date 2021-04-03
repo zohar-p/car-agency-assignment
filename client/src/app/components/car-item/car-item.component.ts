c33be8fa8217dd4f52f7ece366afe304b312c6c1
Zohar Pfeffer <pfzohar@gmail.com>
2021-04-03 11:01:01 +0300

    Display car action only if admin

--- client/src/app/components/car-item/car-item.component.ts
+++ client/src/app/components/car-item/car-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { FormatsService } from 'src/app/formats.service';
+import { UserService } from 'src/app/user.service';
 import { ICar } from '../../car.entity';
 
 @Component({
@@ -10,13 +12,19 @@ import { ICar } from '../../car.entity';
 export class CarItemComponent implements OnInit {
   @Input() car: ICar
   currency: string
+  isAdmin: boolean
+  subscriptions: Subscription[] = []
 
   constructor(
-    private _formatsService: FormatsService
+    private _formatsService: FormatsService,
+    private _userService: UserService,
   ) { }
 
   ngOnInit(): void {
-    this._formatsService.currency$.subscribe(currency => this.currency = currency)
+    this.subscriptions.push(
+      this._formatsService.currency$.subscribe(currency => this.currency = currency),
+      this._userService.isAdmin$.subscribe(isAdmin => this.isAdmin = isAdmin)
+    )
   }
 
 }
