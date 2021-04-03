4ba063e8b614b1be6e551a050a60d470d46c143b
Zohar Pfeffer <pfzohar@gmail.com>
2021-04-03 20:26:49 +0300

    Changed build destination

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ export const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
-app.use(express.static(path.join(__dirname, '/../../public')))
+app.use(express.static(path.join(__dirname, '/client')))
 app.all('*', () => { throw new NotFoundError('Endpoint not found') })
 
 app.use(errorHandler)
